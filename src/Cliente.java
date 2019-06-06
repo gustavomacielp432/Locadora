436dc98d938e1935e71e0323849f83b690e18687
leticiafaria <leticia.faria043@gmail.com>
2019-06-05 22:40:50 -0300

    Ajustes nas validacoes e identação do código

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,43 +1,49 @@
 public class Cliente {
-	
+
 	private String nome;
 	private String cpf;
 	private String endereco;
 	private String dataNasc;
-	
-	
+
 	public Cliente(String nome, String cpf, String endereco, String dataNasc) {
 		this.nome = nome;
 		this.cpf = cpf;
 		this.endereco = endereco;
 		this.dataNasc = dataNasc;
 	}
-	
+
 	public Cliente() {
-		
+
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getEndereco() {
 		return endereco;
 	}
+
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
+
 	public String getDataNasc() {
 		return dataNasc;
 	}
+
 	public void setDataNasc(String dataNasc) {
 		this.dataNasc = dataNasc;
 	}
